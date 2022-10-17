79f2842f7bda84ce269a0efa9d54fa4416f4997e
Iryna Petrenko <iryna.petrenko4@gmail.com>
2022-10-17 13:58:50 +0300

    feat: add solution dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getDNSStats(domains) {
+  let result = {};
+
+  for (let item of domains) {
+    let domainsElem = item.split('.').reverse();
+    let key = '';
+    
+    for (let elem of domainsElem) {
+        key += '.' + elem;
+        result[key] = result[key] ? result[key] + 1 : 1;
+    }
+  }
+  
+  return result;
 }
 
 module.exports = {
